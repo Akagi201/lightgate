6eb637002fc11accdcda7f9ef048bdfe9f4dc443
Akagi201 <akagi201@gmail.com>
2017-02-15 17:04:56 +0800

    feat(cmd): refactor structure

---
+++ cmd/lightgate/init.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/Akagi201/utilgo/conflag"
+	log "github.com/Sirupsen/logrus"
+	"github.com/bshuster-repo/logrus-logstash-hook"
+	"github.com/jessevdk/go-flags"
+)
+
+// all global shared variables
+var (
+	logger *log.Logger
+)
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func init() {
+	parser := flags.NewParser(&opts, flags.Default)
+
+	parser.Parse()
+
+	if opts.Conf != "" {
+		conflag.LongHyphen = true
+		conflag.BoolValue = false
+		args, err := conflag.ArgsFrom(opts.Conf)
+		if err != nil {
+			fmt.Printf("Parse configs from file failed, err: %v", err)
+			os.Exit(-1)
+		}
+		fmt.Printf("args: %v", args)
+		parser.ParseArgs(args)
+	}
+
+	//parser.Parse()
+}
+
+func init() {
+	logger = log.New()
+
+	if level, err := log.ParseLevel(strings.ToLower(opts.LogLevel)); err != nil {
+		logger.Level = level
+	}
+
+	log.SetFormatter(&logrus_logstash.LogstashFormatter{
+		Type:            opts.AppName,
+		TimestampFormat: time.RFC3339Nano,
+	})
+
+	logger.Formatter = &logrus_logstash.LogstashFormatter{
+		Type:            opts.AppName,
+		TimestampFormat: time.RFC3339Nano,
+	}
+}
--- cmd/lightgate/main.go
+++ cmd/lightgate/main.go
@@ -2,76 +2,36 @@ package main
 
 import (
 	"net/http"
-	"runtime"
 
-	"github.com/Akagi201/utilgo/conflag"
-	log "github.com/Sirupsen/logrus"
-	flags "github.com/jessevdk/go-flags"
+	"github.com/Akagi201/light"
+	"github.com/pressly/lg"
 	"github.com/vulcand/oxy/forward"
 	"github.com/vulcand/oxy/testutils"
 )
 
 var opts struct {
 	Conf            string `long:"conf" default:"" description:"config file"`
+	AppName         string `long:"app_name" default:"lightgate" description:"application name"`
 	HTTPListenAddr  string `long:"http_listen" default:"0.0.0.0:8000" description:"HTTP listen address"`
 	WsListenAddr    string `long:"ws_listen" default:"0.0.0.0:8001" description:"WebSocket listen address"`
 	AdminListenAddr string `long:"admin_http_listen" default:"0.0.0.0:8010" description:"Admin HTTP listen address"`
 	LogLevel        string `long:"log_level" default:"info" description:"log level"`
 }
 
-func init() {
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp:   true,
-		TimestampFormat: "2006-01-0¬2 15:04:05",
-	})
-}
-
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	parser := flags.NewParser(&opts, flags.Default)
-
-	parser.Parse()
-	log.Printf("opts: %+v", opts)
-
-	if opts.Conf != "" {
-		conflag.LongHyphen = true
-		conflag.BoolValue = false
-		log.Printf("opts.Conf: %v", opts.Conf)
-		args, err := conflag.ArgsFrom(opts.Conf)
-		if err != nil {
-			log.Fatalf("Parse configs from file failed, err: %v", err)
-		}
-		log.Printf("args: %v", args)
-		parser.ParseArgs(args)
-	}
-
-	parser.Parse()
-
-	log.Printf("opts: %+v", opts)
-
-	if level, err := log.ParseLevel(opts.LogLevel); err != nil {
-		log.SetLevel(level)
-	}
-
-	// reversed proxy logic
-	//log.Fatalln(run())
-}
-
-func run() error {
 	fwd, err := forward.New()
 	if err != nil {
-		return err
+		logger.Fatalf("HTTP forward create failed, err: %v", err)
 	}
 
-	root := http.NewServeMux()
+	root := light.New()
 
-	root.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		r.URL = testutils.ParseURI("http://localhost:8088")
+	root.Use(lg.RequestLogger(logger))
+
+	root.HandleAll("/*path", func(w http.ResponseWriter, r *http.Request) {
+		r.URL = testutils.ParseURI("http://localhost:8327")
 		fwd.ServeHTTP(w, r)
 	})
 
-	http.ListenAndServe(":8080", root)
-
-	return nil
+	http.ListenAndServe(opts.HTTPListenAddr, root)
 }
