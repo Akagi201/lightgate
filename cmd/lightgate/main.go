8d45da1751f9706619e0df59ef47df2f8f27c5c0
Akagi201 <akagi201@gmail.com>
2017-02-16 16:03:43 +0800

    feat(jwt): add jwt auth

--- cmd/lightgate/main.go
+++ cmd/lightgate/main.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/Akagi201/light"
+	jwtmiddleware "github.com/Akagi201/middleware/jwt"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/pressly/lg"
 	"github.com/vulcand/oxy/forward"
 	"github.com/vulcand/oxy/testutils"
@@ -28,10 +30,21 @@ func main() {
 
 	root.Use(lg.RequestLogger(logger))
 
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		Debug: true,
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte("supersecret"), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	root.Use(jwtMiddleware.Handler)
+
 	root.HandleAll("/*path", func(w http.ResponseWriter, r *http.Request) {
 		r.URL = testutils.ParseURI("http://localhost:8327")
 		fwd.ServeHTTP(w, r)
 	})
 
+	logger.Infof("lightgate HTTP listening at %s", opts.HTTPListenAddr)
 	logger.Fatalln(http.ListenAndServe(opts.HTTPListenAddr, root))
 }
